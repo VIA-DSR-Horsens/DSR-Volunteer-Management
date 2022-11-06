c86d7dcf1d6505608eff1f9dfcc151de219fdaf2
Hugo Daniel <82777546+HugoSilva4@users.noreply.github.com>
2022-11-06 23:35:16 +0100

    loginModel
    Login Model using RestAPI

---
+++ loginAPI/src/test/java/org/via/loginapi/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.via.loginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
