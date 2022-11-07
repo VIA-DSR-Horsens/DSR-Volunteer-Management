76cba50fa83b702e9e419c3b6bd9562eeb83da37
Hugo Daniel <82777546+HugoSilva4@users.noreply.github.com>
2022-11-07 19:17:19 +0100

    Api's structure and Login model
    Package for register and login. Login model created, missing documentation.

---
+++ loginAPI/src/main/java/org/via/loginapi/LoginApiApplication.java
@@ -0,0 +1,12 @@
+package org.via.loginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LoginApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApiApplication.class,args);
+    }
+}
