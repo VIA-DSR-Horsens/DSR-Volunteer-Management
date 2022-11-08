7a9aa44f5f90829cf1b304af2a4599678fb441fd
Ieris19 <ieris19@outlook.com>
2022-11-08 10:37:19 +0100

    ProtoBuffer for EventServer
    The protobuf, alongside the dependencies/generated code to go along the Java side of the Event-Manager proof of concept.

---
+++ code/database/database-constructor.sql
@@ -0,0 +1,5 @@
+CREATE ROLE dsr_service
+    LOGIN PASSWORD 'eventManagerSEP';
+CREATE DATABASE dsr;
+CREATE ROLE dsr_admin;
+GRANT ALL PRIVILEGES ON DATABASE dsr TO dsr_admin;
---
+++ code/database/volunteer-DDL.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA IF NOT EXISTS volunteerManager;
+CREATE TABLE IF NOT EXISTS volunteerManager.events(
+    id bigint NOT NULL PRIMARY KEY,
+    name varchar(32) NOT NULL,
+    startTime date NOT NULL,
+    endTime date NOT NULl
+);
+CREATE TABLE IF NOT EXISTS volunteerManager.volunteer(
+    id  bigint NOT NULL PRIMARY KEY,
+    name varchar(60) NOT NULL,
+    shiftsTaken integer DEFAULT 0,
+    rating integer DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS volunteerManager.shifts(
+    id bigint NOT NULL PRIMARY KEY,
+    eventId bigint REFERENCES volunteerManager.events,
+    timestamp varchar(9),
+    volunteerId bigint REFERENCES volunteerManager.volunteer
+);
+GRANT SELECT, INSERT, UPDATE, DELETE
+    ON dsr.volunteermanager.events, dsr.volunteermanager.volunteer, dsr.volunteermanager.shifts
+    TO dsr_service;
