8a2b912ed177a4da6001265eb1218444a89fb515
EmiliaNistor <emilia.georgiana24@gmail.com>
2022-12-07 14:41:40 +0100

    Rebase gRPC progress

--- code/blazor/VolunteerManager/Program.cs
+++ code/blazor/VolunteerManager/Program.cs
@@ -1,5 +1,17 @@
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using VolunteerManager;
 using Microsoft.AspNetCore.Components.Authorization;
 using VolunteerManager.Authentication;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:4566");
+var client = new CreateEvent.CreateEventChannel(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
 
 var builder = WebApplication.CreateBuilder(args);
 
