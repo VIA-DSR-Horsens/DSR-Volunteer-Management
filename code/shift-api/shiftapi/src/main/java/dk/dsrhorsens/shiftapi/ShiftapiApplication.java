a0bd92cacf19f1e77f087f9798a87555e95e42e7
Hugo Daniel <82777546+HugoSilva4@users.noreply.github.com>
2022-12-11 18:48:11 +0100

    Shift Api
    Setting up the package  structure

---
+++ code/shift-api/shiftapi/src/main/java/dk/dsrhorsens/shiftapi/ShiftapiApplication.java
@@ -0,0 +1,13 @@
+package dk.dsrhorsens.shiftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftapiApplication.class, args);
+    }
+
+}
---
+++ code/shift-api/shiftapi/src/test/java/dk/dsrhorsens/shiftapi/ShiftapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.dsrhorsens.shiftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
