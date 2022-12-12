b0330faaaa0d88e55d57dca02548e832cd03fe1a
Hugo Daniel <82777546+HugoSilva4@users.noreply.github.com>
2022-12-12 16:02:15 +0100

    Shift Http Request

--- code/shift-api/shiftapi/src/main/java/dk/dsrhorsens/shiftapi/ShiftapiApplication.java
+++ code/shift-api/shiftapi/src/main/java/dk/dsrhorsens/shiftapi/ShiftapiApplication.java
@@ -1,4 +1,4 @@
-package dk.dsrhorsens.shiftapi;
+package java.dk.dsrhorsens.shiftapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
