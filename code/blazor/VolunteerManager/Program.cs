2562b3c7bc983f1c4941a197e2b870bff8852320
daviskyLV <ledaviskylv@gmail.com>
2022-12-14 20:39:06 +0100

    Added authorization policies

--- code/blazor/VolunteerManager/Program.cs
+++ code/blazor/VolunteerManager/Program.cs
@@ -1,30 +1,11 @@
 using Grpc.Net.Client;
-using VolunteerManager;
 using Microsoft.AspNetCore.Components.Authorization;
 using VolunteerManager.Authentication;
 using VolunteerManager.Services;
-using VolunteerManager.UserProto;
 
 
-Console.WriteLine("press any key to continue...");
-Console.ReadLine();
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:4566");
-// The Localhost refuses to connect if using the code that's commented below. Still haven't figured out why it's not working.
-//var client = new CreateEventService.CreateEventServiceClient(channel);
-//var reply = client.createEvent(new CreateEventInfo
-//{
-//    EventName = "Halloween Party",
-//    EventDate = "28.10.2022",
-//    StartTime = 22,
-//    EndTime = 2,
-//    Location = "VIA University College"
-//});
-//Console.WriteLine("Event Id: " + reply.EventId);
-//Console.WriteLine("press any key to exit");
-//Console.ReadLine();
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -33,6 +14,15 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, SimpleAuthenticationStateProvider>();
 builder.Services.AddScoped<IAuthManager, AuthManagerImpl>();
 builder.Services.AddScoped<IUserService, UserServiceImpl>();
+
+// Authorization policies
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("Manager", a =>
+        a.RequireAuthenticatedUser().RequireClaim("IsManager", true.ToString()));
+    
+    options.AddPolicy("Administrator", a =>
+        a.RequireAuthenticatedUser().RequireClaim("IsAdministrator", true.ToString()));
+});
 
 var app = builder.Build();
 
